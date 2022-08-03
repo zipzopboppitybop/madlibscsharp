f92b1e255d5f2de4041885ec890980d25d65ad5a
Brian Washington <bw49078@gmail.com>
2022-08-03 16:11:49 -0500

    added variables and a story

--- madlibs.cs
+++ madlibs.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace myprogram
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a color: ");
+            string? color = Console.ReadLine();
+            Console.Write("Enter another color: ");
+            string? color2 = Console.ReadLine();
+            Console.Write("Enter something you like: ");
+            string? thing = Console.ReadLine();
+            Console.Write("Enter a name: ");
+            string? person = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine("Violets are " + color2);
+            Console.WriteLine("I like " + thing);
+            Console.WriteLine("I love  " + person);
+        }
+    }
+}
+
